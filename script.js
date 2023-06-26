532be5289afd723218eeb5ec7109d4180c187a3c
Ajay kumar <125947653+ajaykumar230@users.noreply.github.com>
2023-06-26 09:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
-    //Write your code here
+   let item=document.getElementById("form1");
+	var firstname=form1.elements[firstname].value;
+	var lastname=form1.elements[lastname].value;
+
+	alert(firstname+lastname);
+	//Write your code here
 
 }
