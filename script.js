cf96caac6e88462894e4c97d2481e708e06b9320
Ajay kumar <125947653+ajaykumar230@users.noreply.github.com>
2023-06-26 09:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
    let item=document.getElementById("form1");
-	var firstname=form1.elements['firstname'].value;
-	var lastname=form1.elements['lastname'].value;
+	var firstname=form1.elements['fname'].value;
+	var lastname=form1.elements['lname'].value;
 
 	alert(firstname+lastname);
 	//Write your code here
